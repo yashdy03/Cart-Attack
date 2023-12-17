b862e56be896e7e0e21df349e516684c3b5d0244
yashdy03 <yashdy03@gmail.com>
2023-12-17 18:21:54 +0530

    added admin crud

--- src/main/java/com/yas/ecommerce/controller/AdminController.java
+++ src/main/java/com/yas/ecommerce/controller/AdminController.java
@@ -1,28 +1,54 @@
 package com.yas.ecommerce.controller;
 
 import com.yas.ecommerce.model.Category;
+import com.yas.ecommerce.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class AdminController {
+    @Autowired
+    CategoryService categoryService;
     @GetMapping("/admin")
     public String adminHome(){
         return "adminHome";
     }
 
     @GetMapping("/admin/categories")
-    public String categories(){
+    public String categories(Model model){
+        model.addAttribute(categoryService.getAllCategory());
         return "categories";
     }
 
     @GetMapping("/admin/categories/add")
-    public String categoriesAdd(Model model){
+    public String getCategoriesAdd(Model model){
         model.addAttribute("category", new Category());
         return "categoriesAdd";
     }
+    @PostMapping("/admin/categories/add")
+    public String postCategoriesAdd(@ModelAttribute("category") Category category){
+        categoryService.addCategory(category);
+        return "redirect:/admin/categories";
+    }
 
+    @GetMapping("/admin/categories/delete/{id}")
+    public String deleteCategory(@PathVariable int id){
+        categoryService.removeCategoryById(id);
+        return "redirect:/admin/categories";
+    }
+    @GetMapping("/admin/categories/update/{id}")
+    public String updateCategory(@PathVariable int id, Model model){
+        Optional<Category> category = categoryService.getCategoryById(id);
+        if(category.isPresent()){
+            model.addAttribute("category", category.get());
+            return "categoriesAdd";
+        }
+        else
+            return "404";
+    }
 
 }
--- src/main/java/com/yas/ecommerce/service/CategoryService.java
+++ src/main/java/com/yas/ecommerce/service/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -16,5 +17,12 @@ public class CategoryService {
     }
     public List<Category> getAllCategory(){
         return categoryRepository.findAll();
+    }
+
+    public void removeCategoryById(int id){
+        categoryRepository.deleteById(id);
+    }
+    public Optional<Category> getCategoryById(int id){
+        return categoryRepository.findById(id);
     }
 }
